1da00b318b50d2d490c9d7b594104d81d03fe947
Pancho Labrador <plabradorjr@gmail.com>
2019-10-16 02:51:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
 
-  attr_accessors :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
